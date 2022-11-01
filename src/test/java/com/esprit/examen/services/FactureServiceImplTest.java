33941a51445a6988942550d5136f1f0ad6847c09
Seyf_CHERNI <73757234+SeyfCHERNI@users.noreply.github.com>
2022-11-01 23:20:40 +0100

    Update FactureServiceImplTest.java

--- src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import *;
+import tn.esprit.*;
 
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
